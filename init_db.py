68c30b5aed64ea4a022735ee176fd78bdfefb838
qrtman <153613257+qrtman@users.noreply.github.com>
2025-10-02 21:58:01 +0900

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,6 +1,6 @@
 # init_db.py
-from app import init_db
+from telegram_bot import init_db
 
-print("Starting database initialization...")
+print("Starting Telegram bot database initialization...")
 init_db()
 print("Database initialization complete.")
